eabfa85edb026e166b580acc5a929586b41ef7f3
FelipeS02 <felipesaracho.ldv@gmail.com>
2021-08-28 20:06:12 -0300

    first commit

---
+++ api/src/models/Clothe.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  const Clothe = sequelize.define("clothe", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Clothe;
+};
