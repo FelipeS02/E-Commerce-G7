834ae151431a8f16ef11933f9e4143c15d05e73c
Fabian Garcia <gfabian757@gmail.com>
2021-09-01 17:45:35 -0500

    login & logout button fueron creados para auntenticar usuario usando auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/home";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,20 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-    ,
+    <Auth0Provider
+      domain="dev-ls3m5tco.us.auth0.com"
+      clientId="zj1FCNLdMJIfrfMWfYVG4WMHhdgdfUS9"
+      redirectUri={window.location.origin}
+    >
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
