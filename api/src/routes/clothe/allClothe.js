7c5fba7e60b6b240dada5143d3057bf51cd7eff7
FelipeS02 <felipesaracho.ldv@gmail.com>
2021-09-01 00:39:08 -0300

    modify

---
+++ api/src/routes/clothe/allClothe.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+const router = Router();
+const { Clothe, Category } = require("../../db");
+
+let currentOffset = 0 + currentOffset;
+
+router.get("/allClothes", async (_req, res) => {
+  try {
+    const allClothes = await Clothe.findAll({ limit: 10, offset: 0, include: Category });
+    if (allClothes === []) {
+      res.status(404).json({ noClothes: "Aun no existe ninguna prenda" });
+      return;
+    } else {
+      res.status(200).json({ allClothes });
+      return;
+    }
+  } catch (err) {
+    const { message } = err;
+    res.status(400).json({ message });
+    return;
+  }
+});
+
+module.exports = router;
