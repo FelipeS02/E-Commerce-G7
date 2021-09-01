2c0f0257af79a796a84600d2cd32aa6b4476668a
Fabian Garcia <gfabian757@gmail.com>
2021-09-01 09:55:19 -0500

    reducer se configuro con combineReducer para tener un root reducer y porder trabajar con reducers por separado para cada componente

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>         
-            <Route exact path = '/' component= {Home}/>
+        <Switch>
+          <Route exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ client/src/reducer/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  login: {},
+};
+
+function authReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default authReducer;
