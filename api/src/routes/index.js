9114505d1bd56a70ce0adbdaabd1835be543c93c
BrunoBohlmann <brunobohlman25@gmail.com>
2021-08-31 21:54:48 -0300

    agregado de clotheDetail a index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-
 const router = Router();
 
+// Traemos Routes
 const clotheDetail = require('./clotheDetail.js')
 
+// Usamos Routes
+router.use('/clotheDetail', clotheDetail)
 
 module.exports = router;
