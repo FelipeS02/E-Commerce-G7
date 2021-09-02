0e3f245fa07ba13e55f7e883f461938b20e9404f
Fabian Garcia <gfabian757@gmail.com>
2021-09-02 00:19:30 -0500

    NavBar basico fue agregado con la funcion de login y logout usando autencacion con auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/home";
-import Login from "./components/Login/Login";
-
+import NavBar from "./components/NavBar/NavBar";
+import { useAuth0 } from "@auth0/auth0-react";
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
+          <Route exact path="/" component={NavBar} />
           <Route exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
