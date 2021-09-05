2362c9caa0c3d7f8888eddf7a484eafd4d95ae81
FelipeS02 <felipesaracho.ldv@gmail.com>
2021-09-05 17:20:46 -0300

    Arregle la ruta de agregar al carrito

--- api/src/routes/clothe/order/orderAdd.js
+++ api/src/routes/clothe/order/orderAdd.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const { validate } = require("uuid");
-const { Order_clothes, Clothe, User, Order } = require("../../../db");
+const { Clothe, User, Order } = require("../../../db");
 const {
   responseMessage,
   statusCodes: { SUCCESS, ERROR },
@@ -11,12 +11,16 @@ router.post("/order-add/:userId", async (req, res) => {
   try {
     const {
       body: {
-        data: { clothe: { quantity, clotheId, price } = {} },
+        data: {
+          clothe: { quantity, clotheId },
+        },
       },
       params: { userId },
     } = req;
 
     if (clothe && userId && validate(userId) && typeof clothe === "object") {
+      const currentClothe = await Clothe.findByPk(clotheId);
+      //? Busco dentro del usuario las ordenes que tengan status 'CARRITO'
       const userOrder = await User.findAll({
         where: { id: userId },
         include: {
@@ -28,27 +32,29 @@ router.post("/order-add/:userId", async (req, res) => {
       });
 
       if (userOrder.length === 0) {
-        const [user, newOrder, currentClothe] = await Promise.all([
+        //? Si no la encuentra devuelve []
+        const [user, newOrder] = await Promise.all([
           await User.findByPk(userId),
+          //Creo la orden con valor inicial del producto que me llego por id.
           await Order.create({
-            total: price,
+            total: currentClothe.price,
           }),
-          await Clothe.findByPk(clotheId),
         ]);
 
         await Promise.all([
-          await currentClothe.decrement(["stock"], { by: quantity }),
+          //Reduzco el stock por la cantidad
           await newOrder.addClothe(currentClothe, { quantity: quantity }),
           await user.addOrder(newOrder),
         ]);
 
         return res.json(responseMessage(SUCCESS, user));
       } else {
-        const currentOrder = Order.findByPk(userOrder[0].orders[0].id);
-
+        //? Si existe la orden la busco por el id que me trae la relacion
+        await Order.findByPk(userOrder[0].orders[0].id);
         await Promise.all([
+          await currentClothe.decrement(["stock"], { by: quantity }),
           await currentOrder.increment(["total"], { by: price }),
-          await clotheRelation(clothes, currentOrder),
+          await currentOrder.addClothe(currentClothe, { quantity: quantity }),
         ]);
 
         return res.json(responseMessage(SUCCESS, currentOrder));
