ce29e73e6b22b08b7fa3937ca91de933a31f18f5
FelipeS02 <felipesaracho.ldv@gmail.com>
2021-09-05 18:31:28 -0300

    arreglado el pronblema de app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,62 +1,67 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
 const multer = require("multer");
-const path = require('path')
-
+const path = require("path");
 
 // Auth0
-var jwt = require('express-jwt');
-var jwks = require('jwks-rsa');
-var jwtCheck = jwt({
-      secret: jwks.expressJwtSecret({
-          cache: true,
-          rateLimit: true,
-          jwksRequestsPerMinute: 5,
-          jwksUri: 'https://dev-1ik4n80w.us.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'https://ecommerce7',
-    issuer: 'https://dev-1ik4n80w.us.auth0.com/',
-    algorithms: ['RS256']
+let jwt = require("express-jwt");
+let jwks = require("jwks-rsa");
+let jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-1ik4n80w.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "https://ecommerce7",
+  issuer: "https://dev-1ik4n80w.us.auth0.com/",
+  algorithms: ["RS256"],
 });
-app.use(jwtCheck);
 
-require('./db.js');
+require("./db.js");
 
 const server = express();
 
-server.name = 'API';
+server.name = "API";
 
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, 'public/uploads'),
+  destination: path.join(__dirname, "public/uploads"),
   filename: (req, file, cb) => {
     cb(null, file.originalname);
-  }
-})
+  },
+});
 
-server.use(multer({
-  storage,
-  dest: path.join(__dirname, 'public/uploads')
-}).array("media", 8))
+server.use(jwtCheck);
+server.use(
+  multer({
+    storage,
+    dest: path.join(__dirname, "public/uploads"),
+  }).array("media", 8)
+);
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-server.use('/', routes);
+server.use("/", routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
@@ -64,5 +69,3 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 });
 
 module.exports = server;
-
-
