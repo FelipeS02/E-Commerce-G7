01f078abbc12186adc57fd59dd75073e7dd870a2
FelipeS02 <felipesaracho.ldv@gmail.com>
2021-09-05 22:30:11 -0300

    arreglos

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import React, { useEffect, useState } from "react";
-import { Button, Carousel } from "react-bootstrap"
-// import css'
+// import React, { useEffect, useState } from "react";
+// import { Button, Carousel } from "react-bootstrap";
+// // import css'
 
-export default function Product( {id, name, images, price, rating}) {
-   
-    return (
-        <div >
-            <div >
-                <div >
-                   <Carousel>
-                     {images && images.map((item, id) => (
-                      <Item key={id} item={item} />
-                      ))}
-                   </Carousel>
-                </div>
-                <div >
-                    <h2>{name}</h2>
-                    <div >
-                      <p>{HowManyStars(rating)}</p>
-                      <p>{price}</p>
-                    </div>
-                    <div >
-                    <ButtonWishlist
-                    product_id={id}
-                    wishlistProductIDs={wishlistProductIDs}
-                    />
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    );
-}
+// export default function Product({ id, name, images, price, rating }) {
+//   return (
+//     <div>
+//       <div>
+//         <div>
+//           <Carousel>
+//             {images && images.map((item, id) => <Item key={id} item={item} />)}
+//           </Carousel>
+//         </div>
+//         <div>
+//           <h2>{name}</h2>
+//           <div>
+//             <p>{HowManyStars(rating)}</p>
+//             <p>{price}</p>
+//           </div>
+//           <div>
+//             <ButtonWishlist
+//               product_id={id}
+//               wishlistProductIDs={wishlistProductIDs}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
