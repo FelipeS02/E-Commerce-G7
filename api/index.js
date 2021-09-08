287c10bf360990ecbc59c24ae7ce2e4761720af4
FelipeS02 <felipesaracho.ldv@gmail.com>
2021-09-08 16:13:21 -0300

    intento de deploy

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log(`-----------------------------* listening at ${process.env.PORT || 3001} *-----------------------------`); // eslint-disable-line no-console
   });
 });
