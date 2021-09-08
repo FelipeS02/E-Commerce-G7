93be350562244c660c0e7279bc1297aaae82ca91
FelipeS02 <felipesaracho.ldv@gmail.com>
2021-09-08 13:32:54 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.port, () => {
-    console.log(`-----------------------------* listening at ${process.env.port || 3001} *-----------------------------`); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log(`-----------------------------* listening at ${process.env.PORT || 3001} *-----------------------------`); // eslint-disable-line no-console
   });
 });
