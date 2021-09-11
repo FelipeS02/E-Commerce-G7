027706e5e230dcac8a7c19f816c210cd7c2e666e
FelipeS02 <felipesaracho.ldv@gmail.com>
2021-09-11 19:34:24 -0300

    force true

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn
   .sync({
-    force: false,
+    force: true,
   })
   .then(() => {
     server.listen(process.env.PORT || 3001, () => {
