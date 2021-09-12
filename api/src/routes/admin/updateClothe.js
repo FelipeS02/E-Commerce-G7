e83cac20d14692fa1e1214a3e5a340c13ae0761f
Harold Rodriguez <harol20082008@hotmail.com>
2021-09-11 22:12:51 -0500

    ya elimina productos de la base de datos y solo falta que pueda actualizar categorias

--- api/src/routes/admin/updateClothe.js
+++ api/src/routes/admin/updateClothe.js
@@ -1,27 +1,49 @@
 const { Router } = require("express");
 const { Category, Clothe, Media, Type, Size } = require("../../db");
+const { Op } = require("sequelize");
 const router = Router();
 
 const {
-  responseMessage,
-  statusCodes: { SUCCESS, ERROR },
+    responseMessage,
+    statusCodes: { SUCCESS, ERROR },
 } = require("../../controller/responseMessages");
 // en principio, esta ruta puede modificar el nombre, el precio, detalle, stock. Referente 
 // al stock, si ya existe el talle, solo modfica el stock, si no existe el talle, etnonces 
 // lo crea, tanto el talle como el stock. 
+
+router.delete("/:idClothe/delete", async (req,res) => {
+    try {
+        const {idClothe} = req.params;
+
+        Clothe.destroy({
+            where: {
+              id: idClothe,
+            },
+          })
+        return res.json(responseMessage(SUCCESS,{ message : "Clote deleted"}));
+    } catch (err) {
+        const { message } = err;
+        return res.json(responseMessage(ERROR, message));
+    }
+});
+
 router.put("/:idClothe", async (req,res) => {
     try {
         const {idClothe} = req.params;
         const {name, price, sizes, detail, color, genre, categories, type} = req.body;
         
         const clotheToUpdate = await Clothe.findByPk(idClothe,{
-            include :
+            include : [
+            {
+                model: Type,
+                attributes: ["id", "name"]
+            },
             {
                 model: Size,
                 attributes: ["id", "size", "stock"],
                 through: { attributes: [] }
             }
-            });
+            ]});
 
         if(name) clotheToUpdate.name = name;
         if(price) clotheToUpdate.price = price;
@@ -34,6 +56,7 @@ router.put("/:idClothe", async (req,res) => {
 
             arrayKeys.forEach(async e => {
                 const sizeFound = clotheToUpdate.sizes.find(size => size.size === e);
+
                 // si lo encuentra quiere decir que ya existia en la base de datos dicho size.
                 // entonces tenemos que modificar, si no lo encuentra, añadimos un nuevo talle 
                 // a la prenda.
@@ -48,24 +71,26 @@ router.put("/:idClothe", async (req,res) => {
                             id: sizeFound.id
                         }
                     });
-
                 } else {
-                    console.log( e,sizes[e]);
+                    // console.log( e,sizes[e]);
                     const  newSize = await Size.create({size : e , stock : sizes[e]});
-                    console.log(newSize)
+                    // console.log(newSize)
                     await clotheToUpdate.addSize(newSize);
                 }
             });
         }
-        // if(categories){
-
- 
-        // }
+        if(type){
+            await Type.update({
+                name: type
+            },{
+                where : { id : clotheToUpdate.types[0].id }
+            });
+        }
         return res.json(responseMessage(SUCCESS, clotheToUpdate));
     } catch (err) {
         const { message } = err;
         return res.json(responseMessage(ERROR, message));
     }
-})
+});
 
 module.exports = router;
