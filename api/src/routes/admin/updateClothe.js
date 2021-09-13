9842ef65bf89c0c1b16dd3fdbb15647e2510ab61
Harold Rodriguez <harol20082008@hotmail.com>
2021-09-13 00:49:16 -0500

    sweetalert , edit y create temrminado

--- api/src/routes/admin/updateClothe.js
+++ api/src/routes/admin/updateClothe.js
@@ -87,26 +87,14 @@ router.put("/:idClothe", async (req,res) => {
             const arrayKeys = Object.keys(sizeObject);
 
             arrayKeys.forEach(async e => {
-                // const sizeFound = clotheToUpdate.sizes.find(size => size.size === e);
 
                 // si lo encuentra quiere decir que ya existia en la base de datos dicho size.
                 // entonces tenemos que modificar, si no lo encuentra, añadimos un nuevo talle 
                 // a la prenda.
 
-                // if(sizeFound){
-                //         await Size.update({
-                //             size: e,
-                //             stock: sizeObject[e]
-                //         },
-                //         {
-                //             where: {
-                //                 id: sizeFound.id
-                //             }
-                //         });
-                // } else {
                     const  newSize = await Size.create({size : e , stock : sizeObject[e]});
                     await clotheToUpdate.addSize(newSize);
-                // }
+
             });
         }
         if(type){
@@ -118,15 +106,14 @@ router.put("/:idClothe", async (req,res) => {
         }
         if(categories){
             if(!Array.isArray(categories)) {var arrayCategories = categories.split(' ');}
-            // console.log(arrayCategories)
             arrayCategories = categories;
             for(let i = 0; i < clotheToUpdate.categories.length; i++){
                 try {
                     
                     await clothe_category.destroy({where : {
                        
-                            categoryId:clotheToUpdate.categories[i].clothe_category.categoryId,
-                            clotheId:clotheToUpdate.categories[i].clothe_category.clotheId,
+                    categoryId:clotheToUpdate.categories[i].clothe_category.categoryId,
+                    clotheId:clotheToUpdate.categories[i].clothe_category.clotheId,
                         
                     }});
                 } catch (err) {
@@ -136,46 +123,9 @@ router.put("/:idClothe", async (req,res) => {
             }
             arrayCategories.map(async cat => {
                 const newCategory = await Category.create({name : cat});
-                        await clotheToUpdate.addCategory(newCategory);
-                
-                // const categoryFound = await Category.findOne({where :{ name :  cat}});
-                //     if(!categoryFound){
-                //         const newCategory = await Category.create({name : cat});
-                //         await clotheToUpdate.addCategory(newCategory);
-                //     } else {
-                //         await clotheToUpdate.addCategory(categoryFound);
-                //     }
+                await clotheToUpdate.addCategory(newCategory);
             });
         }
-            // if(!Array.isArray(categories)){
-            //     let e = categories;
-            //    for(let i = 0; i < clotheToUpdate.categories.length; i++){
-            //     if(clotheToUpdate.categories[i].name !== e){
-            //         const categoryFound = await Category.findOne({where :{ name :  e}});
-            //         if(!categoryFound){
-            //                 const newCategory = await Category.create({ name : e });
-            //                 await clotheToUpdate.addCategory(newCategory);
-            //         } else {
-            //             await clotheToUpdate.addCategory(categoryFound);
-            //         }
-            //     } 
-            // }
-            // } else {
-            //     categories.forEach(async category => {
-            //         const e = category.toLowerCase();
-            //         for(let i = 0; i < clotheToUpdate.categories.length; i++){
-            //             if(clotheToUpdate.categories[i].name !== e){
-            //                 const categoryFound = await Category.findOne({where :{ name :  e}});
-            //                 if(!categoryFound){
-            //                         const newCategory = await Category.create({ name : e });
-            //                         await clotheToUpdate.addCategory(newCategory);
-            //                 } else {
-            //                     await clotheToUpdate.addCategory(categoryFound);
-            //                 }
-            //             } 
-            //         }
-            // })}
-            
         return res.json(responseMessage(SUCCESS, clotheToUpdate));
     } catch (err) {
         const { message } = err;
